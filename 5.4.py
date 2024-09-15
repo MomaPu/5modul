31a51e7f996ea5750bb766178b14b31a1ccd01ce
DESKTOP-1M5PG59\Matvey <matvey.seymachin@mail.ru>
2024-09-15 21:51:06 +0300

    'Ispravleno'

--- 5.4.py
+++ 5.4.py
@@ -1,21 +1,24 @@
 import json
 
 class Model:
-    title = '1'
-    text = '2'
-    author = '3'
+    def __init__(self, title='1', text='2', author='3'):
+        self.title = title
+        self.text = text
+        self.author = author
 
     def save(self):
+        # Создаем словарь из атрибутов экземпляра
+        data = self.__dict__ # пользовательские
+        data2 = []
+        for x in dir(Model): # запись всех аттрибутов
+            data2.append(x)
+        
+        # Записываем словарь в JSON-файл
         with open("dictionary.json", "w") as write_file:
-            # Сохраняем атрибуты экземпляра в словаре
-            data = {
-                "title": self.title,
-                "text": self.text,
-                "author": self.author
-            }
-            json.dump(data, write_file)
+            json.dump(data, write_file)  # добавление в json
+            json.dump(data2, write_file)
 
+# Создаем экземпляр класса
 M = Model()
-
 # Вызываем метод save для сохранения данных в файл
 M.save()
